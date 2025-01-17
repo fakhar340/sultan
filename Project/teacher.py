a833122634e832e94056390540010bc9811e9f47
AaqibRajput15 <aaqibrajpoot15@gmail.com>
2025-01-17 14:53:43 +0500

    added teacher class file

---
+++ Project/teacher.py
@@ -0,0 +1,14 @@
+class Teacher():
+    def __init__(self,
+        teacher_name,
+        teacher_f_name,
+        teacher_phone_name,
+        teacher_salary,         
+        ):
+        self.teacher_name = teacher_name
+        self.teacher_f_name = teacher_f_name
+        self.teacher_phone_number = teacher_phone_name
+        self.teacher_salary = teacher_salary
+
+    def getteacher(self):
+        return f" Name: {self.teacher_name}  Father Name: {self.teacher_f_name} Phone Number: {self.teacher_phone_number}"   
