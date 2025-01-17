0dde8166a51f2e11416d832138e314e3d9030816
AaqibRajput15 <aaqibrajpoot15@gmail.com>
2025-01-17 15:10:55 +0500

    add all classes in seperate files

---
+++ Project/event.py
@@ -0,0 +1,13 @@
+class Event():
+    def __init__(self, name,date,manager,time,menu, decoration , required_stock, participates, guests ,location,description):
+        self.name = name
+        self.date = date
+        self.manager = manager
+        self.time = time
+        self.menu = menu
+        self.dectoration = decoration
+        self.required_stock = required_stock
+        self.participates = participates
+        self.guests = guests
+        self.location = location
+        self.description = description
---
+++ Project/trade.py
@@ -0,0 +1,16 @@
+class Trades():
+    def __init__(self, AI, Web, MSOFFICE, Digital_Marketing, Industrial_Electrician, Culnary_Art, Fashion_Designing, English, Industrial_stiching, Welding, HVACR, Hair_and_Beauty, AALTP):
+        self, 
+        self.AI = AI, 
+        self.Web = Web, 
+        self.MSOFFICE = MSOFFICE, 
+        self.Digital_Marketing = Digital_Marketing, 
+        self.Industrial_Electrician = Industrial_Electrician, 
+        self.Culnary_Art = Culnary_Art, 
+        self.Fashion_Designing = Fashion_Designing, 
+        self.English = English, 
+        self.Industrial_Electrician = Industrial_stiching, 
+        self.Welding = Welding, 
+        self.HVACR = HVACR, 
+        self.Hair_and_Beauty = Hair_and_Beauty, 
+        self.AALTP = AALTP
